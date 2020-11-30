8bd3eefb5f5c803ecb288a5482efa42033252039
ShyroVO <bosquet.laurie@outlook.com>
2020-11-30 15:17:52 +0100

    1.0

---
+++ app.js
@@ -0,0 +1,11 @@
+let x = 0;
+
+while (x < 10) {
+    console.log("x a la valeur " + x + " lors de sont passage n°" + (x + 1));
+
+    x++;
+}
+
+for (let y = 0; y < 10; y++){
+    console.log("y a la valeur " + y + " lors de sont passage n°" + (y + 1));
+}
